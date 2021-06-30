58aef6c2cc2af5d8b4e1c8c9e45330bd8b6109b8
Flavio <flaviomarcosmarinho@gmail.com>
2021-06-29 21:43:58 -0300

    Exercicio de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1
+while(contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+console.log('Do While')
+
+var contadorDo = 1
+do {
+    console.log(`Passo ${contadorDo}`)
+    contadorDo++
+} while(contadorDo <= 6)
